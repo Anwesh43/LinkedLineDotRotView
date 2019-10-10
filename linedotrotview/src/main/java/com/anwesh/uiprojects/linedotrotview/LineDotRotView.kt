912d3de4f35308836d5f25a00c1bf87e92f20508
Anwesh43 <anweshthecool0@gmail.com>
2019-10-10 10:11:51 +0530

    adding constants and importing libraries

--- linedotrotview/src/main/java/com/anwesh/uiprojects/linedotrotview/LineDotRotView.kt
+++ linedotrotview/src/main/java/com/anwesh/uiprojects/linedotrotview/LineDotRotView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.linedotrotview
 /**
  * Created by anweshmishra on 10/10/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val parts : Int = 3
+val scGap : Float = 0.01f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 2.9f
