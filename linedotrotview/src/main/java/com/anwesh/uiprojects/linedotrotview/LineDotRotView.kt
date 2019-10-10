cef0591f2956be490e476fd9c0069e5ce5e35adf
Anwesh43 <anweshthecool0@gmail.com>
2019-10-10 10:33:22 +0530

    adding LDRNode class

--- linedotrotview/src/main/java/com/anwesh/uiprojects/linedotrotview/LineDotRotView.kt
+++ linedotrotview/src/main/java/com/anwesh/uiprojects/linedotrotview/LineDotRotView.kt
@@ -122,8 +122,50 @@ class LineDotRotView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class LDRNode(var i : Int, val state : State = State()) {
+
+        private var next : LDRNode? = null
+        private var prev : LDRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LDRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDRNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDRNode {
+            var curr : LDRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
         }
     }
 }
