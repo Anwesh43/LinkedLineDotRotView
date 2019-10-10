515e3587112aa2c007bad0ad95ac269f84db6f83
Anwesh43 <anweshthecool0@gmail.com>
2019-10-10 10:26:33 +0530

    adding canvas extension methods

--- linedotrotview/src/main/java/com/anwesh/uiprojects/linedotrotview/LineDotRotView.kt
+++ linedotrotview/src/main/java/com/anwesh/uiprojects/linedotrotview/LineDotRotView.kt
@@ -25,3 +25,38 @@ val rFactor : Float = 2.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineDot(i : Int, sc1 : Float, sc2 : Float, size : Float, paint : Paint) {
+    val r : Float = size / rFactor
+    val deg : Float = 90f / (parts - 1)
+    save()
+    rotate(deg * i)
+    for (j in 0..1) {
+        drawCircle(0f, -size * j * sc1.divideScale(i, parts), r, paint)
+    }
+    drawLine(0f, -r, 0f, -r - (size - 2 * r) * sc2, paint)
+    restore()
+}
+
+fun Canvas.drawLineDots(sc1 : Float, sc2 : Float, size : Float, paint : Paint) {
+    for (j in 0..(parts - 1)) {
+        drawLineDot(j, sc1, sc2, size, paint)
+    }
+}
+
+fun Canvas.drawLDRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE 
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawLineDots(sc1, sc2, size, paint)
+    restore()
+}
